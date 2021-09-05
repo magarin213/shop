3654196fbffb36ebe23529d908585d7671a54d90
magarin <mario213@nate.com>
2021-09-05 20:18:34 +0900

    부트스트랩, import/export, component.map(), router
    부트스트랩 : 버전4
    import/export : js파일 가져오기 내보내기
    component.map() : 컴포넌트화 한걸 반복문
    router : 라우터 설치 react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
-import  { Button }  from  'react-bootstrap' ;
+import { Button, Navbar, Nav, NavDropdown, Container, Jumbotron } from 'react-bootstrap';
+import { useState } from 'react';
+import data from "./data";
+import Detail from './component/view/Detail/Detail';
+import ProductList from "./component/view/ProductList/ProductList"
+import {Link, Route, Switch} from "react-router-dom";
+
 
 function App() {
+
+  const [shoes, setShoes] = useState(data);
+
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{' '}
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link><Link to="/">Home</Link></Nav.Link>
+              <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+<Switch>
+      <Route exact path="/">
+          <Jumbotron className="background">
+            <h1>20% season Off</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+
+          <div className="container">
+          <div className="row">
+            {
+              shoes.map((e, i)=>{
+                return   <ProductList key={i} i={i} shoes={shoes[i]}></ProductList>
+              })
+            }
+          </div>
+          </div>
+        </Route>
+      <Route path="/detail">
+        <Detail shoes={shoes}/>
+      </Route>
+</Switch>
+
+
     </div>
   );
 }
---
+++ src/component/view/ProductList/ProductList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProductList(props) {
+  return (
+    <>
+          <div className="col-md-4">
+          <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="100%"></img>
+          <h4>{props.shoes.title}</h4>
+          <p>{props.shoes.content} & {props.shoes.price}</p>
+          </div>
+    </>
+  )
+}
+
+export default ProductList
