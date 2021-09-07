34c7a374bcf10000cfda53102a6bcff5d0972ac5
magarin <mario213@nate.com>
2021-09-07 19:00:48 +0900

    react-trainsition-group
    npm i react-transition-group
    import {CSSTransition} from 'react-transition-group';
    나머진 내용은 코드 보고 익히기

--- src/component/view/Detail/Detail.js
+++ src/component/view/Detail/Detail.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import {useHistory, useParams} from "react-router-dom"
-import styled from 'styled-components';
+import { Nav } from 'react-bootstrap'
 import '../../../Detail.css'
 import Stock from "./Stock"
+import { CSSTransition } from 'react-transition-group'
 
 function Detail(props) {
 
 const [alert, setAlert] = useState(true);
 const [inputData, setinput] = useState('');
+const [tap, setTap] = useState(0);
+const [switchButton, setSwitchButton] = useState(false);
+
 
 const handler = () => {
   props.setProductamount([9,10,11])
@@ -62,9 +66,38 @@ const inputHandler = (e)=> {
             <button className="btn btn-danger" onClick={BackPage}>뒤로가기</button> 
           </div>
         </div>
+
+        <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+          <Nav.Item>
+            <Nav.Link eventKey="link-0" onClick={()=>{setSwitchButton(false); setTap(0)}}>Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" onClick={()=>{setSwitchButton(false); setTap(1)}}>Option 2</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      <CSSTransition in={switchButton} classNames="animation" timeout={500}>
+        <TabContent tap={tap} setSwitchButton={setSwitchButton} />
+      </CSSTransition>
       </div> 
     </>
   )
+}
+
+
+function TabContent(props) {
+
+  useEffect(() => {
+    props.setSwitchButton(true)
+  }, [])
+
+  if(props.tap === 0){
+     return <div>0번째 내용입니다.</div>
+    } else if(props.tap === 1) {
+      return <div>1번째 내용입니다.</div>
+    }else if(props.tap === 2) {
+    return <div>1번째 내용입니다.</div>
+  }
+
 }
 
 export default Detail
